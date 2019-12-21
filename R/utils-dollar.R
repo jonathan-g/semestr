3d9a225912c08326abb2c6af95590921c5a117bf
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-12-20 22:07:37 -0600

    Add magrittr %$% exposition operator.

---
+++ R/utils-dollar.R
@@ -0,0 +1,11 @@
+#' Exposition operator
+#'
+#' See \code{magrittr::\link[magrittr:exposition]{\%$\%}} for details.
+#'
+#' @name %$%
+#' @rdname exposition
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %$%
+#' @usage lhs \%$\% rhs
+NULL
