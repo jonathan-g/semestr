c4a6093e429cb2522fb7b3d4a34d448482c9faad
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-12-22 00:21:27 -0600

    * Code to prepare the initial database. * Start documenting functions.

--- R/transition_database.R
+++ R/transition_database.R
@@ -1,43 +1,104 @@
 transition.env <- new.env()
 
+#
+# Use as global variables, but encapsulate in an
+# environment.
+#
 with( transition.env, {
+  # Prefixes are prefixed in front of topic keys in the
+  # calendar to identify what kind of item they refer to.
   prefixes <- c(class = "CLS", lab = "LAB", homework = "HW", due_date = "DUE",
                 exam = "EXAM", holiday = "VAC", event = "EVT")
+  # The 1000s place in the calendar id identifies the kind of item
+  # that id refers to.
   bases <- c(class = 1000, lab = 2000, homework = 3000, due_date = 4000,
              exam = 5000, holiday = 6000, event = 7000)
+  # The 100s place in a calendar item is used to indicate
+  # re-scheduling from the regular class time.
   base_mods <- c(cancelled = 100, make_up = 200)
 })
 
-build_base_calendar <- function(master_cal) {
-
-}
-
+#' Extract and index calendar entries by type.
+#'
+#' Extract calendar entries of a given type. Add an index indicating the
+#' sequence of entries with that type, and add a column indicating what
+#' the type is.
+#'
+#' The type is based on the `key_col` argument: Entries with a key in that
+#' column are chosen, and the numerical index column is created using a name
+#' derived by substituting "`_num`" ror "`_key`" in the column name (e.g.,
+#' "`class_key`" becomes "`class_num`"). The index represents the sequence of
+#' events of the chosen type, ordered by date.
+#'
+#' @param df A dataframe holding the calendar. Columns should include:
+#' \describe{
+#'   \item{date}{The date of the calendar item}
+#'   \item{`key_col`}{A character string with the key for the calendar entry.
+#'     This column's name should match the `key_col` argument.}
+#'   \item{cancelled}{An integer indicating whether the entry has been
+#'     cancelled for the date of this entry.}
+#'   \item{make_up}{An integer indicating whether the entry represents a
+#'     re-scheduled make-up event}
+#' }
+#' @param key_col The name of the column with the keys for the entries to be
+#'   selected. This can either be quoted or unquoted (i.e., using non-standard
+#'   tidy evaluation). If the `key_col` argument is missing and `type` is not
+#'   `NULL`, then a default `key_col` will be constructed by appending "`_key`"
+#'   to `type` (e.g., "`class`" becomes "`class_key`").
+#' @param type The type of event. This should be one of `class`, `lab`,
+#'   `homework`, `due date`, `exam`, `holiday`, or `event`.
+#'   If `type` is `NULL`, the value is constructed by removing "`_key`" from
+#'   the end of the `key_col` argument (e.g., "`class_key`" becomes "`class`").
+#'
+#' @return A tibble with columns:
+#' \describe{
+#'   \item{cal_id}{An integer whose thousands place indicates the kind of
+#'     entry, hundreds place indicates re-scheduling (cancellation or make-up),
+#'     and last two digits indicate the order of the entry.}
+#'   \item{date}{The date of the entry, as an ISO format string.}
+#'   \item{cancelled}{An integer indicating whether the entry has been
+#'     cancelled.}
+#'   \item{make_up}{An integer indicating whether the entry represents a
+#'     re-scheduled make-up session.}
+#'   \item{`_key`}{A column with a text key for that entry. The name of this
+#'     column is the `key_col` argument.}
+#'   \item{`_num`}{A column with an integer index representing the order of the
+#'     entry in the sequence of this kind of entry (class, lab, etc.). The
+#'     name of this column is constructed by replacing "`_key`" with "`_num`"
+#'     in the `key_col` argument (e.g., "`class_key`" becomes "`class_num`").}
+#'   \item{cal_type}{A string indicating the type of entry (class, lab, etc.)}
+#' }
+#'
 do_index <- function(df, key_col, type = NULL) {
   if (missing(key_col)) {
     key_col <- stringr::str_c(type, "_key")
   }
   qcol <- enquo(key_col)
-  message("enquosed")
+  # message("enquosed")
   print(qcol)
   if (is_character(quo_get_expr(qcol))) {
-    message("character")
+    # message("character")
     key <- key_col
     key_col <- ensym(key_col)
     qcol <- enquo(key_col)
   } else {
-    message("not character")
+    # message("not character")
     key <- as.character(quo_get_expr(qcol))
   }
   key <- stringr::str_replace(key, "_key$", "")
-  message("key = ", key)
+  # message("key = ", key)
   if (is.null(type)) {
     type <- key
+  } else {
+    if (! str_replace_all(type, " +", "_") %in% names(prefixes)) {
+      stop('Bad type name "', type, '".')
+    }
   }
   num_col_name <- stringr::str_c(key, "_num")
   num_col <- ensym(num_col_name)
   qnum <- enquo(num_col)
   if (as.character(quo_get_expr(qcol)) %in% names(df)) {
-    df <- df %>% dplyr::select(cal_id, date, !!qcol, cancelled, make_up) %>%
+    df <- df %>% dplyr::select(date, !!qcol, cancelled, make_up) %>%
       dplyr::arrange(date) %>%
       dplyr::filter(!is.na(!!qcol)) %>%
       dplyr::mutate(cal_type = type,
@@ -56,6 +117,44 @@ do_index <- function(df, key_col, type = NULL) {
   invisible(df)
 }
 
+#' Re-index calendar entries of a single type.
+#'
+#' Take a calendar with entries of a given type and re-index them so that
+#' repeated entries with the same text key get the same numerical index.
+#' For instance, when there is a week-long holiday, such as Thanksgiving or
+#' Spring Break, there may be two or three entries with the same key, but on
+#' different dates. This function makes sure that they are all assigned the
+#' same numerical index.
+#'
+#' @param df A dataframe holding calendar events of a given type.
+#'   Columns should include:
+#' \describe{
+#'   \item{cal_id}{An integer whose thousands place indicates the kind of
+#'     entry, hundreds place indicates re-scheduling (cancellation or make-up),
+#'     and last two digits indicate the order of the entry.}
+#'   \item{date}{The date of the entry, as an ISO format string.}
+#'   \item{cancelled}{An integer indicating whether the entry has been
+#'     cancelled.}
+#'   \item{make_up}{An integer indicating whether the entry represents a
+#'     re-scheduled make-up session.}
+#'   \item{`key_col`}{A column with a text key for that entry.}
+#'   \item{`idx_col`}{A column with an integer index representing the sequence
+#'     of this kind of entry (class, lab, etc.)}
+#'   \item{cal_type}{A string indicating the type of entry (class, lab, etc.)}
+#' }
+#' @param key_col The name of the column with the keys for the entries.
+#'   This can either be quoted or unquoted (i.e., using non-standard tidy
+#'   evaluation).
+#' @param idx_col The name of the column with the integer index for entries
+#'   `homework`, `due date`, `exam`, `holiday`, or `event`.
+#'   If `idx_col` is `NULL`, the value is constructed by replacing "`_key`" with
+#'   "`_num`" in the `key_col` argument (e.g., "`class_key`" becomes
+#'   "`class_num`").
+#'
+#' @return A tibble with the same columns as the input `df`, but with the
+#'   numerical index in `idx_col` adjusted so multiple entries with the same
+#'   key in `key_col` have the same numerical index in `idx_col`.
+#'
 reindex <- function(df, key_col, idx_col = NULL) {
   qkcol <- enquo(key_col)
   if (is_character(quo_get_expr(qkcol))) {
@@ -89,6 +188,51 @@ reindex <- function(df, key_col, idx_col = NULL) {
   invisible(dfx)
 }
 
+#' Convert an event-specific calendar tibble to a general master calendar.
+#'
+#' Convert the columns of a calendar specific to a certain kind of entry to
+#' generic columns suitable for a master calendar containing multiple kinds of
+#' entries.
+#'
+#' @param df A dataframe holding the calendar. Columns should include:
+#' \describe{
+#'   \item{date}{The date of the calendar item}
+#'   \item{`key_col`}{A character string with the key for the calendar entry.
+#'     This column's name should match the `key_col` argument.}
+#'   \item{cancelled}{An integer indicating whether the entry has been
+#'     cancelled for the date of this entry.}
+#'   \item{make_up}{An integer indicating whether the entry represents a
+#'     re-scheduled make-up event}
+#' }
+#' @param key_col The name of the column with the keys for the entries to be
+#'   selected. This can either be quoted or unquoted (i.e., using non-standard
+#'   tidy evaluation). If the `key_col` argument is missing and `type` is not
+#'   `NULL`, then a default `key_col` will be constructed by appending "`_key`"
+#'   to `type` (e.g., "`class`" becomes "`class_key`").
+#' @param type The type of event. This should be one of `class`, `lab`,
+#'   `homework`, `due date`, `exam`, `holiday`, or `event`.
+#'   If `type` is `NULL`, the value is constructed by removing "`_key`" from
+#'   the end of the `key_col` argument (e.g., "`class_key`" becomes "`class`").
+#'
+#' @return A tibble with columns:
+#' \describe{
+#'   \item{cal_id}{An integer whose thousands place indicates the kind of
+#'     entry, hundreds place indicates re-scheduling (cancellation or make-up),
+#'     and last two digits indicate the order of the entry.}
+#'   \item{date}{The date of the entry, as an ISO format string.}
+#'   \item{cancelled}{An integer indicating whether the entry has been
+#'     cancelled.}
+#'   \item{make_up}{An integer indicating whether the entry represents a
+#'     re-scheduled make-up session.}
+#'   \item{topic_key}{A column with a text key for that entry. }
+#'   \item{cal_type}{A string indicating the type of entry (class, lab, etc.)}
+#'   \item{...}{Other entries, such as a numerical index for events of a
+#'     given type. In general, we expect (but do not require) that there is
+#'     a numerical index column corresponding to the `key_col` argument
+#'     (e.g., if `key_col` = "`lab_key`", then there should be a "`lab_num`"
+#'     column corresponding to the sequence of laboratory entries).}
+#' }
+#'
 cal_prepare <- function(df, key_col, type = NULL) {
   if (missing(key_col)) {
     key_col <- stringr::str_c(type, "_key")
--- R/utils-tidy-eval.R
+++ R/utils-tidy-eval.R
@@ -1,5 +1,3 @@
-#' Tidy eval helpers
-#'
 #' @importFrom rlang expr enquo enquos ensym ensyms is_character quo_get_expr
 #'             sym syms .data := as_name as_label !! !!!
 NULL
