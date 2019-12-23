1e301953231d85d135777c0de7990d530148c29c
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-12-23 01:49:26 -0600

    Update transition_database script.

--- R/transition_database.R
+++ R/transition_database.R
@@ -1,11 +1,19 @@
-transition.env <- new.env()
+transition_env <- new.env()
 
 #
 # Use as global variables, but encapsulate in an
 # environment.
 #
-with( transition.env, {
-  # Prefixes are prefixed in front of topic keys in the
+with( transition_env, {
+  type2idx <- c(class = "class", lab = "lab", homework = "homework",
+                "due date" = "due_date", exam = "exam", holiday = "holiday",
+                event = "event")
+  type2col <- c(class = "class", lab = "lab", homework = "hw",
+                "due date" = "due", exam = "exam", holiday = "holiday",
+                event = "evt")
+  col2type <- set_names(names(type2col), type2col)
+  idx2type <- set_names(names(type2idx), type2idx)
+  # Prefixes are prefixed in front of topic keys in thelow
   # calendar to identify what kind of item they refer to.
   prefixes <- c(class = "CLS", lab = "LAB", homework = "HW", due_date = "DUE",
                 exam = "EXAM", holiday = "VAC", event = "EVT")
@@ -17,6 +25,12 @@ with( transition.env, {
   # re-scheduling from the regular class time.
   base_mods <- c(cancelled = 100, make_up = 200)
 })
+
+pull_env <- function(env) {
+  for (n in ls(env)) {
+    assign(n, get(n, envir = env), envir = parent.frame())
+  }
+}
 
 #' Extract and index calendar entries by type.
 #'
@@ -69,13 +83,20 @@ with( transition.env, {
 #'   \item{cal_type}{A string indicating the type of entry (class, lab, etc.)}
 #' }
 #'
-do_index <- function(df, key_col, type = NULL) {
-  if (missing(key_col)) {
-    key_col <- stringr::str_c(type, "_key")
+do_index <- function(df, type, key_col = NULL) {
+  pull_env(transition_env)
+
+  assert_that(! all(is.null(key_col), is.null(type)),
+              msg = "Either type or key_col must be specified in do_index.")
+
+  if (is.null(key_col)) {
+    assert_that(type %in% col2type,
+                msg = str_c('Bad type name "', type, '" in do_index.'))
+    key_col <- stringr::str_c(type2col[type], "_key")
   }
   qcol <- enquo(key_col)
   # message("enquosed")
-  print(qcol)
+  # print(qcol)
   if (is_character(quo_get_expr(qcol))) {
     # message("character")
     key <- key_col
@@ -88,12 +109,10 @@ do_index <- function(df, key_col, type = NULL) {
   key <- stringr::str_replace(key, "_key$", "")
   # message("key = ", key)
   if (is.null(type)) {
-    type <- key
-  } else {
-    if (! str_replace_all(type, " +", "_") %in% names(prefixes)) {
-      stop('Bad type name "', type, '".')
-    }
+    assertthat(key %in% type2col)
+    type <- col2type[key]
   }
+
   num_col_name <- stringr::str_c(key, "_num")
   num_col <- ensym(num_col_name)
   qnum <- enquo(num_col)
@@ -155,7 +174,19 @@ do_index <- function(df, key_col, type = NULL) {
 #'   numerical index in `idx_col` adjusted so multiple entries with the same
 #'   key in `key_col` have the same numerical index in `idx_col`.
 #'
-reindex <- function(df, key_col, idx_col = NULL) {
+reindex <- function(df, type, key_col = NULL, idx_col = NULL) {
+  pull_env(transition_env)
+
+  assert_that(! all(is.null(type), is.null(key_col)),
+              msg = "Either type or key_col must be specified in reindex.")
+
+  if (is.null(key_col)) {
+    assert_that(type %in% col2type,
+                msg = str_c('Bad type name "', type, '" in reindex.'))
+
+    key_col <- str_c(type2col[type], "_key")
+  }
+
   qkcol <- enquo(key_col)
   if (is_character(quo_get_expr(qkcol))) {
     key = key_col
@@ -233,9 +264,16 @@ reindex <- function(df, key_col, idx_col = NULL) {
 #'     column corresponding to the sequence of laboratory entries).}
 #' }
 #'
-cal_prepare <- function(df, key_col, type = NULL) {
-  if (missing(key_col)) {
-    key_col <- stringr::str_c(type, "_key")
+cal_prepare <- function(df, type, key_col = NULL) {
+  pull_env(transition_env)
+
+  assert_that(! all(is.null(type), is.null(key_col)),
+              msg = "Either type or key_col must be specified in cal_prepare")
+
+  if (is.null(key_col)) {
+    assert_that(type %in% col2type,
+                msg = str_c('Bad type name: "', type, '" in cal_prepare'))
+    key_col <- stringr::str_c(type2col[type], "_key")
   }
   qcol <- enquo(key_col)
   # message("enquosed")
@@ -252,7 +290,8 @@ cal_prepare <- function(df, key_col, type = NULL) {
   key <- stringr::str_replace(key, "_key$", "")
   # message("key = ", key)
   if (is.null(type)) {
-    type <- key
+    assertthat(key %in% type2col)
+    type <- col2type[key]
   }
   prefix <- prefixes[type]
 
@@ -265,61 +304,100 @@ cal_prepare <- function(df, key_col, type = NULL) {
 }
 
 
-transition_database <- function(dest_file = "semester.sqlite3",
-                                master_file = "alt_semester.sqlite3",
-                                base_file = "new_semester.sqlite3") {
-  base_dir <- rprojroot::find_root(rprojroot::has_file("DESCRIPTION"))
-  planning_dir <- file.path(base_dir, "planning")
+build_master_calendar <- function(master_db_file) {
+  pull_env(transition_env)
 
-  attach(transition.env)
-
-  master_db <- DBI::dbConnect(RSQLite::SQLite(),
-                              file.path(planning_dir, master_file))
+  master_db <- DBI::dbConnect(RSQLite::SQLite(), master_db_file)
 
   cal <- dplyr::tbl(master_db, "master_calendar") %>% dplyr::collect()
 
-  base_classes <- cal %>% do_index(class_key) %>% reindex(class_key) %>%
+  base_classes <- cal %>% do_index("class") %>% reindex("class") %>%
     dplyr::mutate(week = class_num %/% 3 + 1)
-  labs <- cal %>% do_index(lab_key) %>% reindex(lab_key)
-  exams <- cal %>% do_index(exam_key) %>% reindex(exam_key)
-  homework <- cal %>% do_index(hw_key, "homework") %>% reindex(event_key)
-  due_dates <- cal %>% do_index(due_key, "due_date") %>% reindex(due_key)
-  holidays <- cal %>% do_index(holiday_key) %>% reindex(holiday_key)
-  events <- cal %>% do_index(event_key) %>% reindex(event_key)
+  labs <- cal %>% do_index("lab") %>% reindex("lab")
+  exams <- cal %>% do_index("exam") %>% reindex("exam")
+  homework <- cal %>% do_index("homework") %>% reindex("homework")
+  due_dates <- cal %>% do_index("due date") %>% reindex("due date")
+  holidays <- cal %>% do_index("holiday") %>% reindex("holiday")
+  events <- cal %>% do_index("event") %>% reindex("event")
 
   calendar <- bind_rows(
-    cal_prepare(base_classes, class_key),
-    cal_prepare(labs, lab_key),
-    cal_prepare(exams, exam_key),
-    cal_prepare(homework, hw_key, "homework"),
-    cal_prepare(due_dates, due_key, "due_date"),
-    cal_prepare(holidays, holiday_key),
-    cal_prepare(events, event_key)
+    cal_prepare(base_classes, "class"),
+    cal_prepare(labs, "lab"),
+    cal_prepare(exams, "exam"),
+    cal_prepare(homework, "homework"),
+    cal_prepare(due_dates, "due date"),
+    cal_prepare(holidays, "holiday"),
+    cal_prepare(events, "event")
   ) %>% dplyr::arrange(date, cal_id) %>%
     dplyr::select(cal_id, date, class_num, week, topic_key, cal_type,
                   cancelled, make_up, everything())
 
-  classes <- cal %>% do_index(class_key) %>% reindex(class_key) %>%
+  classes <- cal %>% do_index("class") %>% reindex("class") %>%
     dplyr::mutate(week = class_num %/% 3 + 1)
 
-  labs <- cal %>% do_index(lab_key) %>% reindex(lab_key)
-  exams <- cal %>% do_index(exam_key) %>% reindex(exam_key)
-  homework <- cal %>% do_index(hw_key, "homework") %>% reindex(event_key)
-  due_dates <- cal %>% do_index(due_key, "due_date") %>% reindex(due_key)
-  holidays <- cal %>% do_index(holiday_key) %>% reindex(holiday_key)
-  events <- cal %>% do_index(event_key) %>% reindex(event_key)
+  labs <- cal %>% do_index("lab") %>% reindex("lab")
+  exams <- cal %>% do_index("exam") %>% reindex("exam")
+  homework <- cal %>% do_index("homework") %>% reindex("homework")
+  due_dates <- cal %>% do_index("due date") %>% reindex("due date")
+  holidays <- cal %>% do_index("holiday") %>% reindex("holiday")
+  events <- cal %>% do_index("event") %>% reindex("event")
 
   calendar <- bind_rows(
-    cal_prepare(classes, class_key),
-    cal_prepare(labs, lab_key),
-    cal_prepare(exams, exam_key),
-    cal_prepare(homework, hw_key, "homework"),
-    cal_prepare(due_dates, due_key, "due_date"),
-    cal_prepare(holidays, holiday_key),
-    cal_prepare(events, event_key)
+    cal_prepare(classes, "class"),
+    cal_prepare(labs, "lab"),
+    cal_prepare(exams, "exam"),
+    cal_prepare(homework, "homework"),
+    cal_prepare(due_dates, "due date"),
+    cal_prepare(holidays, "holiday"),
+    cal_prepare(events, "event")
   ) %>% dplyr::arrange(date, cal_id) %>%
     dplyr::select(cal_id, date, class_num, week, topic_key, cal_type,
                   cancelled, make_up, everything())
+
+  invisible(calendar)
+}
+
+strip_topic_key <- function(df, type) {
+  pull_env(transition_env)
+
+  target <- str_c("^", prefixes[type], "_")
+  df <- df %>% mutate(topic_key = str_replace(topic_key, target, ""))
+  invisible(df)
+}
+
+extract_links <- function(df, type, num_col = NULL) {
+  pull_env(transition_env)
+
+  if (is.null(num_col)) {
+    assert_that(type %in% col2type,
+                msg = str_c('Bad type name "', type, '" in extract_links.'))
+    num_col <- str_c(type2col[type], "_num")
+  }
+  num_col <- ensym(num_col)
+  df <- df %>% filter(cal_type == type) %>%
+    select(cal_id, topic_key, !!num_col) %>%
+    strip_topic_key(type)
+  invisible(df)
+}
+
+build_database <- function(dest_db_file, master_db_file, base_db_file) {
+  pull_env(transition_env)
+
+  master_calendar <- build_master_calendar(master_db_file)
+
+  calendar <- master_calendar %>%
+    select(cal_id, date, class_num, week, topic_key, cal_type, cancelled,
+           make_up) %>% arrange(date, cal_id, cal_type)
+
+  class_links   <- extract_links(master_calendar, "class"   )
+  lab_links     <- extract_links(master_calendar, "lab"     )
+  hw_links      <- extract_links(master_calendar, "homework")
+  exam_links    <- extract_links(master_calendar, "exam"    )
+  due_links     <- extract_links(master_calendar, "due date")
+  holiday_links <- extract_links(master_calendar, "holiday" )
+  event_links   <- extract_links(master_calendar, "event"   )
+
+
 }
 
 
